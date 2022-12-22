9fe46cdae479f7a4e3fdf335618a9079c96952c5
mzgdev <mzambrano595@gmail.com>
2022-12-21 20:54:47 -0400

    Actualizados paquetes de Tailwind y solucionando problema del plugin con vscode

--- pages/index.vue
+++ pages/index.vue
@@ -25,38 +25,7 @@ useSchemaOrg([
 
 // ColorMode
 const colorMode = useColorMode();
-const themes = [
-  "system",
-  "light",
-  "dark",
-  "cupcake",
-  "bumblebee",
-  "emerald",
-  "corporate",
-  "synthwave",
-  "retro",
-  "cyberpunk",
-  "valentine",
-  "halloween",
-  "garden",
-  "forest",
-  "aqua",
-  "lofi",
-  "pastel",
-  "fantasy",
-  "wireframe",
-  "black",
-  "luxury",
-  "dracula",
-  "cmyk",
-  "autumn",
-  "business",
-  "acid",
-  "lemonade",
-  "night",
-  "coffee",
-  "winter",
-];
+const themes = ["mytheme", "corporate"]
 </script>
 
 <template>
